7b2cac4d5a543ae40662b3be8e54ef56ec347f55
xJustFox <dodydaversa@gmail.com>
2024-01-31 15:46:26 +0100

    [Upgrade] Created parent class 'Product

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    // CLASSE GENITORE
+    class Product{
+        public $name;
+        public $img;
+        public $price;
+        public $description;
+
+        public function __construct($_name,$_img, $_price, $_description){
+            $this->name = $_name;
+            $this->img = $_img;
+            $this->price = $_price;
+            $this->description = $_description;
+        }
+    }
+?>
